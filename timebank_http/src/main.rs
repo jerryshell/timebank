2c5827ac32cd1ba810d800d9c5ffa45ed423feff
jerry <calljerryli@outlook.com>
2022-11-28 03:58:58 +0800

    cors

--- timebank_http/src/main.rs
+++ timebank_http/src/main.rs
@@ -3,6 +3,7 @@ use axum::{routing::get, Router};
 use std::net::SocketAddr;
 use std::sync::Arc;
 use timebank_http::*;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -12,11 +13,17 @@ async fn main() {
 
     tracing_subscriber::fmt::init();
 
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/record/list", get(get_record_list))
         .route("/record/search", post(search))
         .route("/record/create", post(create))
-        .with_state(shared_state);
+        .with_state(shared_state)
+        .layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::info!("listening on {}", addr);
