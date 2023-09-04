befef768973eb596fd2d83350219ce101efb52ed
jerry <calljerryli@outlook.com>
2023-09-04 18:57:57 +0800

    日志滚动分割

--- restart.sh
+++ restart.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
+
 export PORT=3000
+
 export ADMIN_TOKEN="YOUR_TOKEN"
+
+export RUST_LOG=info
+
 BASE_PATH=$(pwd)
+
 BIN_FILENAME=$1
-LOG_FILENAME="$BIN_FILENAME.$(date -u +%Y-%m-%dT%H:%M:%SZ).log"
+
 BIN_PATH="$BASE_PATH/$BIN_FILENAME"
-LOG_PATH="$BASE_PATH/$LOG_FILENAME"
+
+LOG_FILENAME="$BIN_FILENAME.$(date -u +%Y-%m-%dT%H_%M_%SZ).log"
+
+# LOG_PATH="$BASE_PATH/$LOG_FILENAME"
+LOG_PATH="/dev/null"
+
 CMD="$BIN_PATH"
-KILL_CMD="ps aux | grep '$CMD' | grep -v grep | awk '{print \$2}' | xargs kill -9"
+
+KILL_CMD="pidof '$CMD' | xargs kill -9"
+
+bash -c "chmod +x $1"
+
 bash -c "$KILL_CMD"
+
 bash -c "nohup $CMD > $LOG_PATH 2>&1 &"
--- timebank_http/src/main.rs
+++ timebank_http/src/main.rs
@@ -1,7 +1,13 @@
 #[tokio::main]
 async fn main() {
     // init tracing
-    tracing_subscriber::fmt::init();
+    let file_appender = tracing_appender::rolling::daily("./", "timebank_http.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt().with_writer(non_blocking).init();
+
+    // admin token
+    let admin_token = std::env::var("ADMIN_TOKEN").unwrap_or("admin_token".to_string());
+    tracing::info!(admin_token);
 
     // init db
     let db_pool = timebank_db::init_sqlite_db()
@@ -39,22 +45,26 @@ async fn main() {
         .layer(axum::Extension(ip_to_admin_token_error_count_map))
         .layer(cors);
 
+    // init ip addr
+    let ip_addr = std::env::var("IP_ADDR")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(std::net::IpAddr::V4(std::net::Ipv4Addr::new(0, 0, 0, 0)));
+
     // init port
     let port = std::env::var("PORT")
         .ok()
         .and_then(|s| s.parse().ok())
-        .unwrap_or(3000);
-    tracing::info!(port);
+        .unwrap_or(20000);
+    tracing::info!("port {}", port);
 
-    // admin token
-    let admin_token = std::env::var("ADMIN_TOKEN").unwrap_or("admin_token".to_string());
-    tracing::info!(admin_token);
+    // init socket addr
+    let socket_addr = std::net::SocketAddr::new(ip_addr, port);
+    tracing::info!("socket_addr {}", socket_addr);
 
     // run app
-    let addr = std::net::SocketAddr::from(([0, 0, 0, 0], port));
-    tracing::info!("addr={}", addr);
-    axum::Server::bind(&addr)
+    axum::Server::bind(&socket_addr)
         .serve(app.into_make_service_with_connect_info::<std::net::SocketAddr>())
         .await
-        .expect("axum::Server::bind().serve() err");
+        .expect("axum serve err");
 }
